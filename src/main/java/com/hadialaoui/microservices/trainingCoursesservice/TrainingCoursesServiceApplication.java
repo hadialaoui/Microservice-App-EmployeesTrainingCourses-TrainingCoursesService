da1d5d99b71182cbf3fcfab19ee84673c43ad942
hadialaoui <a.hadialaoui@gmail.com>
2019-10-29 00:10:06 +0100

    connecting employess microservice to Eureka

--- src/main/java/com/hadialaoui/microservices/trainingCoursesservice/TrainingCoursesServiceApplication.java
+++ src/main/java/com/hadialaoui/microservices/trainingCoursesservice/TrainingCoursesServiceApplication.java
@@ -2,8 +2,10 @@ package com.hadialaoui.microservices.trainingCoursesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TrainingCoursesServiceApplication {
 
 	public static void main(String[] args) {
